ce4949b6790a5e51fd8e24e35bba757e4884f29a
Abhishek Kumar <abhishek.kumar@adesa.com>
2020-05-02 15:03:09 +0530

    first commit

---
+++ src/com/abhishek/SelectionSort/Main.java
@@ -0,0 +1,25 @@
+package com.abhishek.SelectionSort;
+import java.util.Arrays;
+public class Main {
+    public static void main(String[] args) {
+        int intArray[] = {20, 35, -15, 7, 55, 1, -22};
+        for (int lastUnsortedIndex = intArray.length - 1; lastUnsortedIndex > 0; lastUnsortedIndex--) {
+            int largest = 0;
+            for (int i = 1; i <= lastUnsortedIndex; i++) {
+                if (intArray[i] > intArray[largest]) {
+                    largest = i;
+                }
+            }
+            swap(intArray, largest, lastUnsortedIndex);
+        }
+        System.out.println(Arrays.toString(intArray));
+    }
+    private static void swap(int[] intArray, int i, int j) {
+        if (i == j) {
+            return;
+        }
+        int temp = intArray[i];
+        intArray[i] = intArray[j];
+        intArray[j] = temp;
+    }
+}
